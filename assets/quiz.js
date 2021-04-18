f162ea7df4795355d6d807d7d834850fb7a2fbad
shanemcf <shanemcfadden95@yahoo.com>
2021-04-18 11:02:43 -0500

    adding functionality

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,1 +1,9 @@
-//js
+//creating variables to store 
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+// create questions for quiz
+let questions = [
+
+]
